38c7dec680549044a23299d1926111f951687b0f
Thomaz <115989472+Thomaz-Dantass@users.noreply.github.com>
2022-10-31 11:08:32 -0300

    commit inicial

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach(aba => {
+    aba.addEventListener("click", function() {
+
+        if(aba.classList.contains("selecionado")){
+            return;
+        }
+
+        const abaSelecionada = document.querySelector(".aba.selecionado");
+        abaSelecionada.classList.remove("selecionado");
+
+        aba.classList.add("selecionado");
+
+        const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+        informacaoSelecionada.classList.remove("selecionado");
+
+        const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+
+        const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
+        informacaoASerMostrada.classList.add("selecionado")
+    });
+});
